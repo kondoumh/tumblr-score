6342a50c2dcb0a98e3b96712d938c4e8bd4b1f56
kondoumh <kondoumh@gmail.com>
2019-06-17 01:29:20 +0900

    refactor

--- main.js
+++ main.js
@@ -12,39 +12,37 @@ const performance = require('performance-now');
 let targetPosts = [];
 
 const fetchBlog = async() => {
-    await fetch(`${baseurl}${identifier}/info?api_key=${apiKey}`).then(response => response.json()).then(json => {
-        const blog = json['response']['blog'];
-        console.log(`title: ${blog['title']}`);
-        console.log(`posts: ${blog['posts']}`);
-        console.log(`url: ${blog['url']}`);
-    });
+    const response = await fetch(`${baseurl}${identifier}/info?api_key=${apiKey}`);
+    const json = await response.json();
+    const blog = json['response']['blog'];
+    console.log(`title: ${blog['title']}`);
+    console.log(`posts: ${blog['posts']}`);
+    console.log(`url: ${blog['url']}`);
 }
 
 const getPostCount = async() => {
-    const count = await fetch(`${baseurl}${identifier}/info?api_key=${apiKey}`)
-        .then(response => response.json()).then(json => {
-            return json['response']['blog']['posts'];
-        });
+    const response = await fetch(`${baseurl}${identifier}/info?api_key=${apiKey}`);
+    const json = await response.json();
+    const count = json['response']['blog']['posts'];
     return parseInt(count);
 }
 
 const fetchPosts = async(type, offset, minCount = 0) => {
-    await fetch(`${baseurl}${identifier}/posts/${type}?notes_info=true&reblog_info=true&offset=${offset}&api_key=${apiKey}`)
-        .then(response => response.json()).then(json => {
-            process.stdout.write('\r' + offset);
-            _.each(json['response']['posts'], function(post) {
-                if (!post['reblogged_root_name'] && parseInt(post['note_count']) >= minCount) {
-                    const postInfo = {
-                        url: `'https://${identifier}/post/${post['id']}'`,
-                        date: `'${post['date']}'`,
-                        type: `'${post['type']}'`,
-                        slug: `'${post['slug']}'`,
-                        count: `'${post['note_count']}'`
-                    };
-                    targetPosts.push(postInfo)
-                }
-            });
-        });
+    const response = await fetch(`${baseurl}${identifier}/posts/${type}?notes_info=true&reblog_info=true&offset=${offset}&api_key=${apiKey}`);
+    const json = await response.json();
+    process.stdout.write('\r' + offset);
+    _.each(json['response']['posts'], (post) => {
+        if (!post['reblogged_root_name'] && parseInt(post['note_count']) >= minCount) {
+            const postInfo = {
+                url: `'https://${identifier}/post/${post['id']}'`,
+                date: `'${post['date']}'`,
+                type: `'${post['type']}'`,
+                slug: `'${post['slug']}'`,
+                count: `'${post['note_count']}'`
+            };
+            targetPosts.push(postInfo)
+        }
+    })
 }
 
 const output = (path, data) => {
@@ -63,19 +61,18 @@ const fetchMyPosts = async() => {
         tasks.push(fetchPosts('', offset, 2));
         offset += 20;
     }
-    Promise.all(tasks).then(() => {
-        console.log('\n' + (performance() - start_ms).toFixed(3) + ' elapsed.');
-        const fileName = 'tumblr-score-' + moment().format("YYYYMMDDHHmmss");
-        let csv = ''
-        targetPosts.sort((a, b) => {
-            if (a.date > b.date) return -1;
-            if (a.date < b.date) return 1;
-            return 0;
-        });
-        _.each(targetPosts, (post) => { csv += `${post.url},${post.date},${post.type},${post.slug},${post.count}\n` });
-        output(`work/${fileName}.csv`, csv);
-        output(`work/${fileName}.json`, JSON.stringify(targetPosts, null, 2))
+    await Promise.all(tasks);
+    console.log('\n' + (performance() - start_ms).toFixed(3) + ' elapsed.');
+    const fileName = 'tumblr-score-' + moment().format("YYYYMMDDHHmmss");
+    let csv = ''
+    targetPosts.sort((a, b) => {
+        if (a.date > b.date) return -1;
+        if (a.date < b.date) return 1;
+        return 0;
     });
+    _.each(targetPosts, (post) => { csv += `${post.url},${post.date},${post.type},${post.slug},${post.count}\n` });
+    output(`work/${fileName}.csv`, csv);
+    output(`work/${fileName}.json`, JSON.stringify(targetPosts, null, 2));
 }
 
 fetchMyPosts();
